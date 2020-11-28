9f9652999cdc3e824cad7b2a8f8f3f7e2d8aa88a
Erik Thorselius <erik.thorselius@gmail.com>
2020-11-28 13:39:30 +0100

    save last-update as ZonedDateTime

--- awesome-toggle.clj
+++ awesome-toggle.clj
@@ -1,60 +1,54 @@
 (require '[clojure.java.shell :refer [sh]])
-(defn concentrate [state]
-      (shell/sh "hueadm" "light" "1" "white" "sat=53" "hue=33016" "bri=254" "ct=233") (assoc state :state :concentrate))
-(defn off [state]
-      (shell/sh "hueadm" "light" "1" "off") (assoc state :state :off))
-(defn relax [state]
-      (shell/sh "hueadm" "light" "1" "white" "ct=447" "hue=33016" "bri=144" "sat=53") (assoc state :state :relax))
-(defn night-light [state]
-      (shell/sh "hueadm" "light" "1" "red" "bri=100") (assoc state :state :night-light))
-(defn to-date-time [date]
-      (let [inst (. date toInstant)
-            zone (java.time.ZoneId/of "Europe/Stockholm")]
-           (java.time.ZonedDateTime/ofInstant inst zone)))
-(defn to-date [date-time]
-      (. java.util.Date from (. date-time toInstant)))
-(defn save-state [state]
-      (spit "/tmp/awesome-toggle.tmp"
-            (-> state
-                (update :last-update to-date)
-                (pr-str))))
-(defn last-state []
-      (let [filename "/tmp/awesome-toggle.tmp"
-            init-state {:state :concentrate :last-update (-> "#inst \"1970-01-01T00:00:00.000-00:00\"" (edn/read-string) (to-date-time))}]
-           (try
-             (-> (slurp filename)
-                 (edn/read-string)
-                 (update :last-update to-date-time))
-             (catch Exception e
-               (do (save-state init-state)
-                   init-state)))))
+; read and write ZonedDateTime to file
+(defmethod print-method java.time.ZonedDateTime [v ^java.io.Writer w]
+           (.write w (format "#java/zoned[\"%s\"]" v)))
+
+(defn parse-zoned [^java.time.ZonedDateTime v]
+      (java.time.ZonedDateTime/parse v))
+
+(defn read-zoned [v]
+      (parse-zoned (first v)))
+
+(set! *data-readers*
+      (assoc *data-readers* 'java/zoned read-zoned))
+
+(defn save-state [filename state]
+      (spit filename (pr-str state))
+      state)
+
+(defn load-state [filename]
+      (try
+        (edn/read-string {:readers *data-readers*} (slurp filename))
+        (catch Exception e
+          (save-state filename {:state :concentrate :last-update #java/zoned["1970-01-01T01:00+01:00[Europe/Stockholm]"]}))))
+
+; force concentrate first time on workdays
 (defn now []
       (-> (java.time.ZonedDateTime/now)
           (.withZoneSameInstant (java.time.ZoneId/of "Europe/Stockholm"))))
-(defn toggle [last-state]
-      (case (:state last-state)
-            :off (concentrate last-state)
-            :concentrate (relax last-state)
-            :relax (night-light last-state)
-            :night-light (off last-state)))
+
 (defn- day-of-week []
        (-> (now)
            (.getDayOfWeek)
            (.toString)))
+
 (defn workday? []
       (case (day-of-week)
             "SUNDAY" false
             "SATURDAY" false
             true))
+
 (defn morning? []
       (->> (now)
            (.getHour)
            (>= 6 12)))
+
 (defn not-today? [date-time]
       (-> (now)
           (. toLocalDate)
           (. equals (. date-time toLocalDate))
           (not)))
+
 (defn force-concentrate?
       [state]
       (if (and
@@ -63,8 +57,41 @@
         (assoc state :state :off)
         state))
 
-(-> (last-state)
-    (force-concentrate?)
-    (toggle)
-    (assoc :last-update (now))
-    (save-state))
+; commands
+(defn concentrate []
+      (shell/sh "hueadm" "light" "1" "white" "sat=53" "hue=33016" "bri=254" "ct=233"))
+
+(defn off []
+      (shell/sh "hueadm" "light" "1" "off"))
+
+(defn relax []
+      (shell/sh "hueadm" "light" "1" "white" "ct=447" "hue=33016" "bri=144" "sat=53"))
+
+(defn night-light []
+      (shell/sh "hueadm" "light" "1" "red" "bri=100"))
+
+(defn toggle [state]
+      (let [update-state-to (partial assoc state :state)]
+           (case (:state state)
+                 :off (update-state-to :concentrate)
+                 :concentrate (update-state-to :relax)
+                 :relax (update-state-to :night-light)
+                 :night-light (update-state-to :off))))
+
+(defn run-command [state]
+      (do (case (:state state)
+                :off (off)
+                :concentrate (concentrate)
+                :relax (relax)
+                :night-light (night-light))
+          state))
+
+(defn last-update [state]
+      (assoc state :last-update (now)))
+
+(->> (load-state "/tmp/awesome-toggle.tmp")
+     (force-concentrate?)
+     (toggle)
+     (run-command)
+     (last-update)
+     (save-state "/tmp/awesome-toggle.tmp"))
